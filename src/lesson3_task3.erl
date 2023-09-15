8709bcc6d46f7f44ef44664a111cafb5aae17b05
Alex Gofman <o.v.gofman@gmail.com>
2023-09-15 23:28:39 +0300

    Task3. String to List of Bins

--- src/lesson3_task3.erl
+++ src/lesson3_task3.erl
@@ -2,7 +2,7 @@
 %%% @author alex
 %%% @copyright (C) 2023
 %%% @doc
-%%%
+%%% Maps String to List of Words w/ Specified Delimiter
 %%% @end
 %%% Created : 05 Sep 2023 1:16 PM
 %%%-------------------------------------------------------------------
@@ -14,21 +14,33 @@
   split/2     % <<"Col1-:-Col2-:-Col3-:-Col4-:-Col5">> -> [<<"Col1">>, <<"Col2">>, <<"Col3">>, <<"Col4">>, <<"Col5">>]
 ]).
 
-
-split(Text, Delimiter) ->
-  loop(Text, Delimiter, []).
-
-loop(T, Delimiter, R) ->
-  case T of
-    <<X:3/binary, T/binary>> when X =:= Delimiter -> X;
-    _ -> 2
+split(String, Delimiter) ->
+  reverse(split(String, Delimiter, len(Delimiter), <<>>, [])).
+split(<<>>, <<>>, _, _, _) -> [];
+split(String, <<>>, _, _, _) -> [String];
+split(<<>>, _, _, <<>>, Result_list) -> Result_list;
+split(<<>>, _, _, Word, Result_list) -> [Word | Result_list];
+split(String, D, DL, Word, Result_list) ->
+  case String of
+    <<X:DL/binary, Rest/binary>> when X =:= D, Word =:= <<>> -> split(Rest, D, DL, <<>>, Result_list);
+    <<X:DL/binary, Rest/binary>> when X =:= D -> split(Rest, D, DL, <<>>, [Word | Result_list]);
+    <<H, Rest/binary>> -> split(Rest, D, DL, <<Word/binary, H>>, Result_list);
+    <<H>> -> split(<<>>, D, DL, <<>>, [H | Result_list])
   end.
 
+reverse(L) ->
+  reverse(L, []).
+reverse(L, R) ->
+  case L of
+    [H | []] -> [H | R];
+    [H | T] -> reverse(T, [H | R]);
+    [] -> R
+  end.
 
-%% @spec 1. Returns the very last elem of the list
-%%last(L) ->
-%%  case L of
-%%    [H | []] -> H;
-%%    [_ | T] -> last(T);
-%%    _ -> []
-%%  end.
+len(L) ->
+  len(L, 0).
+len(L, N) ->
+  case L of
+    <<_, T/binary>> -> len(T, N + 1);
+    _ -> N
+  end.
